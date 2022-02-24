e1e9c546af69583250535abef2d4ece0491fa492
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-02-25 00:38:17 +0400

    feat: add index.js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const bracketsArr = [];
+  for(let i = 0; i < bracketsConfig.length; i++){
+    bracketsArr[i] = bracketsConfig[i].join('')
+  }
+  for(let j = 0; j < bracketsArr.length;){
+    if(str.indexOf(bracketsArr[j]) >= 0) {
+      str = str.replace(bracketsArr[j], '')
+      j = 0;
+    }
+    else {
+      j++;
+    }
+  }
+  if(str.length === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
